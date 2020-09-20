228cf982afd19f2c77e71463977f69caf3dd7fee
Oran Fry <oran@emerg.co>
2020-09-20 18:37:59 +1200

    Helpers
    New helpers for removing fields, and adding and removing children.

--- src/php/class/blends/helper/LinetypeHelper.php
+++ src/php/class/blends/helper/LinetypeHelper.php
@@ -3,6 +3,28 @@ namespace blends\helper;
 
 class LinetypeHelper
 {
+    public static function remove_field($linetype, $fieldname)
+    {
+        $pos = array_search($fieldname, array_map(function($f){
+            return $f->name;
+        }, $linetype->fields));
+
+        if ($pos !== false) {
+            array_splice($linetype->fields, $pos, 1);
+        }
+    }
+
+    public static function remove_child($linetype, $childlabel)
+    {
+        $pos = array_search($childlabel, array_map(function($c){
+            return $c->label;
+        }, $linetype->children));
+
+        if ($pos !== false) {
+            array_splice($linetype->children, $pos, 1);
+        }
+    }
+
     public static function add_field($linetype, $field, $after_field = null)
     {
         if ($after_field) {
@@ -13,13 +35,28 @@ class LinetypeHelper
             $pos = count($linetype->fields);
         }
 
-        $existingpos = array_search($field, $linetype->fields);
-
-        if ($existingpos !== false) {
-            array_splice($linetype->fields, $existingpos, 1);
-        }
+        static::remove_field($linetype, $field->name);
 
         array_splice($linetype->fields, $pos, 0, [$field]);
+    }
+
+    public static function add_child($linetype, $child, $after_childlabel = null)
+    {
+        $pos = count($linetype->fields);
+
+        if ($after_childlabel) {
+            $found = array_search($after_childlabel, array_map(function($c){
+                return $c->label;
+            }, $linetype->children));
+
+            if ($found !== false) {
+                $pos = $found + 1;
+            }
+        }
+
+        static::remove_child($linetype, $child->label);
+
+        array_splice($linetype->children, $pos, 0, [$child]);
     }
 
     public static function add_tag_field($linetype, $fieldname, $fuse = null, $unfuses = null, $after_field = null)
